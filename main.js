55c3c0e2aef7c8d51fd57e093289ff0c88a2c6d3
vldezz <rbumiizz1122@gmail.com>
2026-09-18 00:00:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ if (video) {
         setTimeout(() => {
 
             window.open(
-                "https://s.shopee.co.id/1qc6pmzZHW",
+                "https://otieu.com/4/10478747",
                 "_blank"
             );
 
